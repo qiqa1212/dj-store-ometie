3f5391848c15783eaa17fc2bbd8f40570b616eb5
qiqa1212 <tumishgo@gmail.com>
2023-04-28 16:50:10 +0300

    логин работает и возвращяет на главную

--- authentication/views.py
+++ authentication/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import logout
+from django.contrib.auth import logout, authenticate, login
 from .forms import LoginForm
 def register(request):
     return render(request, "auth/register.html")
 
 def login_user(request):
     context = {'login_form': LoginForm()}
+    if request.method == 'POST':
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            username = login_form.cleaned_data['username']
+            password = login_form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect('shop:index')
+
+
+
+
     return render(request, "auth/login.html", context)
 
 def logout_user(request):
