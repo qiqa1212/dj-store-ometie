5cd126c52177a18b8d765141306748921a79cc21
qiqa1212 <tumishgo@gmail.com>
2023-04-28 21:59:41 +0300

    register end

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,7 +6,7 @@ app_name = 'auth'
 
 urlpatterns = [
     
-    path('register/', views.register, name='register'),
+    path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
 ]
